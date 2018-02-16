f765e50be6dea9986992ad7383299270b9f30049
YuriNem <PsychoactivePie@gmail.com>
2018-02-16 10:13:42 +0300

    Add Balance

--- src/games/balance.js
+++ src/games/balance.js
@@ -2,44 +2,100 @@ import cons from '../cons';
 import { flow } from '..';
 
 const askBalance = () => {
-  const number = Math.floor(Math.random() * (9000 + 1)) + 1000;
+  const number = Math.floor(Math.random() * (10000 - 10)) + 10;
   const resultInQuestion = `${number}`;
-  let numberOne = Math.floor(number / 1000);
-  let numberTwo = Math.floor((number - (numberOne * 1000)) / 100);
-  let numberThree = Math.floor((number - (numberOne * 1000) - (numberTwo * 100)) / 10);
-  let numberFour = Math.floor(number - (numberOne * 1000) - (numberTwo * 100) -
-  (numberThree * 10));
-  let counter = 0;
-  while (counter !== 4) {
-    counter = 0;
-    if ((numberFour - 1) > numberThree) {
-      numberFour -= 1;
-      numberThree += 1;
-      counter += 1;
+  let resultInAnswer = '';
+  if (number < 100) {
+    let numberOne = Math.floor(number / 10);
+    let numberTwo = Math.floor(number - (numberOne * 10));
+    let counter = 1;
+    while (counter !== 0) {
+      counter = 1;
+      if ((numberTwo - 1) > numberOne) {
+        numberTwo -= 1;
+        numberOne += 1;
+        counter += 1;
+      }
+      if ((numberOne - 1) > numberTwo) {
+        numberOne -= 1;
+        numberTwo += 1;
+        counter += 1;
+      }
+      if (counter === 1) {
+        counter -= 1;
+      }
     }
-    if ((numberThree - 1) > numberTwo) {
-      numberThree -= 1;
-      numberTwo += 1;
-      counter += 1;
+    const arrayNumbers = [numberOne, numberTwo];
+    arrayNumbers.sort();
+    resultInAnswer = String(arrayNumbers[0]) + String(arrayNumbers[1]);
+  } else if (number < 1000) {
+    let numberOne = Math.floor(number / 100);
+    let numberTwo = Math.floor((number - (numberOne * 100)) / 10);
+    let numberThree = Math.floor(number - (numberOne * 100) - (numberTwo * 10));
+    let counter = 1;
+    while (counter !== 0) {
+      counter = 1;
+      if ((numberThree - 1) > numberTwo) {
+        numberThree -= 1;
+        numberTwo += 1;
+        counter += 1;
+      }
+      if ((numberTwo - 1) > numberOne) {
+        numberTwo -= 1;
+        numberOne += 1;
+        counter += 1;
+      }
+      if ((numberOne - 1) > numberThree) {
+        numberOne -= 1;
+        numberThree += 1;
+        counter += 1;
+      }
+      if (counter === 1) {
+        counter -= 1;
+      }
     }
-    if ((numberTwo - 1) > numberOne) {
-      numberTwo -= 1;
-      numberOne += 1;
-      counter += 1;
+    const arrayNumbers = [numberOne, numberTwo, numberThree];
+    arrayNumbers.sort();
+    resultInAnswer = String(arrayNumbers[0]) + String(arrayNumbers[1]) +
+    String(arrayNumbers[2]);
+  } else {
+    let numberOne = Math.floor(number / 1000);
+    let numberTwo = Math.floor((number - (numberOne * 1000)) / 100);
+    let numberThree = Math.floor((number - (numberOne * 1000) - (numberTwo * 100)) / 10);
+    let numberFour = Math.floor(number - (numberOne * 1000) - (numberTwo * 100) -
+    (numberThree * 10));
+    let counter = 1;
+    while (counter !== 0) {
+      counter = 1;
+      if ((numberFour - 1) > numberThree) {
+        numberFour -= 1;
+        numberThree += 1;
+        counter += 1;
+      }
+      if ((numberThree - 1) > numberTwo) {
+        numberThree -= 1;
+        numberTwo += 1;
+        counter += 1;
+      }
+      if ((numberTwo - 1) > numberOne) {
+        numberTwo -= 1;
+        numberOne += 1;
+        counter += 1;
+      }
+      if ((numberOne - 1) > numberFour) {
+        numberOne -= 1;
+        numberFour += 1;
+        counter += 1;
+      }
+      if (counter === 1) {
+        counter -= 1;
+      }
     }
-    if ((numberOne - 1) > numberFour) {
-      numberOne -= 1;
-      numberFour += 1;
-      counter += 1;
-    }
-    if (counter === 0) {
-      break;
-    }
+    const arrayNumbers = [numberOne, numberTwo, numberThree, numberFour];
+    arrayNumbers.sort();
+    resultInAnswer = String(arrayNumbers[0]) + String(arrayNumbers[1]) +
+    String(arrayNumbers[2]) + String(arrayNumbers[3]);
   }
-  const arrayNumbers = [numberOne, numberTwo, numberThree, numberFour];
-  arrayNumbers.sort();
-  const resultInAnswer = String(arrayNumbers[0]) + String(arrayNumbers[1]) +
-  String(arrayNumbers[2]) + String(arrayNumbers[3]);
   return cons(resultInQuestion, resultInAnswer);
 };
 
