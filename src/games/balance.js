01d52656f553df7c902f5ac826541824a7f29697
YuriNem <PsychoactivePie@gmail.com>
2018-07-07 18:42:20 +0300

    Changed balance.js

--- src/games/balance.js
+++ src/games/balance.js
@@ -2,100 +2,26 @@ import cons from '../cons';
 import { flow } from '..';
 
 const askBalance = () => {
-  const number = Math.floor(Math.random() * (10000 - 10)) + 10;
+  let number = Math.floor(Math.random() * (10000 - 10)) + 10;
   const userQuestion = `${number}`;
-  let userAnswer = '';
-  if (number < 100) {
-    let numberOne = Math.floor(number / 10);
-    let numberTwo = Math.floor(number - (numberOne * 10));
-    let counter = 1;
-    while (counter !== 0) {
-      counter = 1;
-      if ((numberTwo - 1) > numberOne) {
-        numberTwo -= 1;
-        numberOne += 1;
-        counter += 1;
-      }
-      if ((numberOne - 1) > numberTwo) {
-        numberOne -= 1;
-        numberTwo += 1;
-        counter += 1;
-      }
-      if (counter === 1) {
-        counter -= 1;
-      }
-    }
-    const arrayNumbers = [numberOne, numberTwo];
-    arrayNumbers.sort();
-    userAnswer = String(arrayNumbers[0]) + String(arrayNumbers[1]);
-  } else if (number < 1000) {
-    let numberOne = Math.floor(number / 100);
-    let numberTwo = Math.floor((number - (numberOne * 100)) / 10);
-    let numberThree = Math.floor(number - (numberOne * 100) - (numberTwo * 10));
-    let counter = 1;
-    while (counter !== 0) {
-      counter = 1;
-      if ((numberThree - 1) > numberTwo) {
-        numberThree -= 1;
-        numberTwo += 1;
-        counter += 1;
-      }
-      if ((numberTwo - 1) > numberOne) {
-        numberTwo -= 1;
-        numberOne += 1;
-        counter += 1;
-      }
-      if ((numberOne - 1) > numberThree) {
-        numberOne -= 1;
-        numberThree += 1;
-        counter += 1;
-      }
-      if (counter === 1) {
-        counter -= 1;
-      }
-    }
-    const arrayNumbers = [numberOne, numberTwo, numberThree];
-    arrayNumbers.sort();
-    userAnswer = String(arrayNumbers[0]) + String(arrayNumbers[1]) +
-    String(arrayNumbers[2]);
-  } else {
-    let numberOne = Math.floor(number / 1000);
-    let numberTwo = Math.floor((number - (numberOne * 1000)) / 100);
-    let numberThree = Math.floor((number - (numberOne * 1000) - (numberTwo * 100)) / 10);
-    let numberFour = Math.floor(number - (numberOne * 1000) - (numberTwo * 100) -
-    (numberThree * 10));
-    let counter = 1;
-    while (counter !== 0) {
-      counter = 1;
-      if ((numberFour - 1) > numberThree) {
-        numberFour -= 1;
-        numberThree += 1;
-        counter += 1;
-      }
-      if ((numberThree - 1) > numberTwo) {
-        numberThree -= 1;
-        numberTwo += 1;
-        counter += 1;
-      }
-      if ((numberTwo - 1) > numberOne) {
-        numberTwo -= 1;
-        numberOne += 1;
-        counter += 1;
-      }
-      if ((numberOne - 1) > numberFour) {
-        numberOne -= 1;
-        numberFour += 1;
-        counter += 1;
-      }
-      if (counter === 1) {
-        counter -= 1;
-      }
-    }
-    const arrayNumbers = [numberOne, numberTwo, numberThree, numberFour];
-    arrayNumbers.sort();
-    userAnswer = String(arrayNumbers[0]) + String(arrayNumbers[1]) +
-    String(arrayNumbers[2]) + String(arrayNumbers[3]);
+  let sum = 0;
+  let count = 0;
+  while (number) {
+    sum += number % 10;
+    count += 1;
+    number = Math.floor(number / 10);
   }
+  const value = Math.floor(sum / count);
+  let rest = sum % count;
+  let result = 0;
+  for (let i = 0; i < count; i += 1) {
+    result += value * (10 ** i);
+    if (rest) {
+      result += 1 * (10 ** i);
+      rest -= 1;
+    }
+  }
+  const userAnswer = `${result}`;
   return cons(userQuestion, userAnswer);
 };
 
